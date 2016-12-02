08ea10974f488385ffa6ecacf78be6957ed84ce4
Scott Smith <mail@ScottHSmith.com>
2016-12-01 23:51:23 -0800

    Improved front-end script.

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -1,8 +1,10 @@
-boomsvgloader.load('/static/images/icons/icon-sprite.svg');
+'use strict';
 
 /* Global Variables */
-var get_url = '/api/v1.0/get_responses?format=display'
+var get_url_display = '/api/v1.0/get_responses?format=display'
+var get_url = '/api/v1.0/get_responses?format=json'
 var post_url = '/api/v1.0/add_response'
+var response_json
 
 /* Page Elements */
 var responseForm = document.querySelector('.input-form');
@@ -24,22 +26,41 @@ var responseContainer = document.querySelector('.response-text');
 }(this, function() {
 	'use strict';
 
+  function layout(responses) {
+    var content = ''
+    for (var r in responses) {
+      content = content + responses[r] + ', ';
+    }
+    content = content.replace(/,\s*$/, '');
+    responseContainer.innerHTML = content
+  }
+
+  function handle_form(event) {
+    event = event || window.event;
+    event.preventDefault();
+    var targ = event.target || event.srcElement;
+    var form = targ;
+    var data = new FormData(form);
+    scottis.send_response(post_url, data);
+    form.reset();
+  }
+
 	function load_responses(url) {
 		var httpRequest = new XMLHttpRequest();
 		httpRequest.open('GET', url, true);
 		httpRequest.send();
 		httpRequest.onload = function(e) {
 			// console.log(httpRequest.responseText)
-			responseContainer.innerHTML = httpRequest.responseText;
+      response_json = JSON.parse(httpRequest.responseText);
+      scottis.layout(response_json);
+			// responseContainer.innerHTML = httpRequest.responseText;
 		};
 	}
 
 	function send_response(url, data) {
-    console.log('sending');
 		var httpRequest = new XMLHttpRequest();
 		httpRequest.open('POST', url, true);
     httpRequest.onreadystatechange = function() {
-      console.log('callback.');
       after_response(httpRequest)
     };
 		// httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'); This causes HUGE problems.
@@ -48,11 +69,8 @@ var responseContainer = document.querySelector('.response-text');
 	}
 
   function after_response(httpRequest) {
-    console.log('after_response');
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      console.log('Waiting for 202.');
       if (httpRequest.status === 202) {
-        console.log('Reloading responses.');
         load_responses(get_url)
       } else {
         console.log('There was a problem with the request.');
@@ -61,22 +79,14 @@ var responseContainer = document.querySelector('.response-text');
   }
 
 	return {
+    layout: layout,
+		handle_form: handle_form,
 		load_responses: load_responses,
 		send_response: send_response
 	};
 }));
 
 /* Client Actions */
-function handle_form(event) {
-  console.log('handle_form');
-  event = event || window.event;
-  event.preventDefault();
-  var targ = event.target || event.srcElement;
-  var form = targ;
-  var data = new FormData(form);
-  scottis.send_response(post_url, data);
-  form.reset();
-}
-
+boomsvgloader.load('/static/images/icons/icon-sprite.svg');
 scottis.load_responses(get_url);
-responseForm.addEventListener('submit', handle_form);
+responseForm.addEventListener('submit', scottis.handle_form);
