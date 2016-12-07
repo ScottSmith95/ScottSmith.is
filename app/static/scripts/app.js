580eb1bc304a3482ad96ca15b49023cdf332c88c
Scott Smith <mail@ScottHSmith.com>
2016-12-06 17:23:31 -0800

    Added status feature.

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -29,10 +29,44 @@ var responseContainer = document.querySelector('.response-text');
   function layout(responses) {
     var content = ''
     for (var r in responses) {
+      // Concatenate and comma separate strings.
       content = content + responses[r] + ', ';
     }
+    // Remove final string's comma.
     content = content.replace(/,\s*$/, '');
     responseContainer.innerHTML = content
+  }
+
+  function make_alert(text, type) {
+    if (type === undefined) {
+        type = 'neutral';
+    }
+
+    if (document.getElementById('status')) {
+      var statusEl = document.getElementById('status');
+      statusEl.className = '';
+      statusEl.classList.add('status', type)
+      statusEl.innerHTML = text
+    } else {
+      var alert_html = "<div id='status' class='status " + type + "' role='alert' aria-live='assertive'><p>"
+        + text
+        + "</p></div>";
+      responseContainer.insertAdjacentHTML('afterend', alert_html);
+    }
+
+    // Remove alert after 3s.
+    setTimeout(function() {
+      scottis.remove_alert();
+    }, 4000);
+  }
+
+  function remove_alert() {
+    var statusEl = document.getElementById('status');
+    statusEl.classList.add('remove');
+
+    setTimeout(function() {
+      statusEl.remove();
+    }, 1000);
   }
 
   function handle_form(event) {
@@ -71,8 +105,10 @@ var responseContainer = document.querySelector('.response-text');
   function after_response(httpRequest) {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 202) {
+        scottis.make_alert("Success! Message posted. 👍", 'success');
         load_responses(get_url)
       } else {
+        scottis.make_alert("Uh small problem. Tell me about it. 😕", 'failure');
         console.log('There was a problem with the request.');
       }
     }
@@ -80,6 +116,8 @@ var responseContainer = document.querySelector('.response-text');
 
 	return {
     layout: layout,
+    make_alert: make_alert,
+    remove_alert: remove_alert,
 		handle_form: handle_form,
 		load_responses: load_responses,
 		send_response: send_response
@@ -90,3 +128,4 @@ var responseContainer = document.querySelector('.response-text');
 boomsvgloader.load('/static/images/icons/icon-sprite.svg');
 scottis.load_responses(get_url);
 responseForm.addEventListener('submit', scottis.handle_form);
+// scottis.make_alert("Hi Emma. 👍", 'success');
