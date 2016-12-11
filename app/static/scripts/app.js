45d90a957a0fb4b5d9c6cdeff46579da5445b891
Scott Smith <mail@ScottHSmith.com>
2016-12-11 01:16:58 -0800

    New, word cloud style is better.

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -41,80 +41,25 @@ var submitButton = document.querySelector('.input-form input[type="submit"]');
 		var response_number = Object.keys(responses).length
 		console.log(response_number);
 
-		// Create an Array of random positions.
-		var positions = [];
-		positions = generatePositionsArray(100, 100, 5, 1);
-
 		// Position each response with a random position.
 		for (var r in responses) {
 			// Add content divs
 			var response_html = "<li class='response' id='" + r + "'>" + responses[r] + "</li>";
 			response_container.insertAdjacentHTML('afterbegin', response_html);
 			var el = document.getElementById(r);
-			position(el, positions);
+			word_cloud(el);
 		}
 	}
 
-	// Returns a random integer between min (included) and max (excluded)
-	// Using Math.round() will give you a non-uniform distribution!
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min)) + min;
+	function getRandomIntInclusive(min, max) {
+	  min = Math.ceil(min);
+	  max = Math.floor(max);
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
-	// generate random positions
-	function generatePositionsArray(maxX, maxY, safeRadius, irregularity) {
-		// declarations
-		var positionsArray = [];
-		var r, c;
-		var rows;
-		var columns;
-		// count the amount of rows and columns
-		rows = Math.floor(maxY / safeRadius);
-		columns = Math.floor(maxX / safeRadius);
-		// loop through rows
-		for (r = 1; r <= rows; r += 1) {
-			// loop through columns
-			for (c = 1; c <= columns; c += 1) {
-				// populate array with point object
-				positionsArray.push({
-					x: Math.round(maxX * c / columns) + getRandomInt(irregularity * -1, irregularity),
-					y: Math.round(maxY * r / rows) + getRandomInt(irregularity * -1, irregularity)
-				});
-			}
-		}
-		// return array
-		return positionsArray;
-	}
-
-	// get random position from positions array
-	function getRandomPosition(array, removeTaken) {
-		// declarations
-		var randomIndex;
-		var coordinates;
-		// get random index
-		randomIndex = getRandomInt(0, array.length - 1);
-		// get random item from array
-		coordinates = array[randomIndex];
-		// check if remove taken
-		if (removeTaken) {
-			// remove element from array
-			array.splice(randomIndex, 1);
-		}
-		// return position
-		return coordinates;
-	}
-
-	function position(el, positions) {
-		var pos = getRandomPosition(positions, true);
-		console.log(pos);
-		var elWidth = el.clientWidth;
-		var elHeight = el.clientHeight;
-		var posx = pos['x'];
-		var posy = pos['y'];
-
-		el.style.position = 'absolute';
-		el.style.left = 'calc('+posx+'% - ' + elWidth + 'px)';
-		el.style.top = 'calc('+posy+'% - ' + elHeight + 'px)';
+	function word_cloud(el) {
+		var mod = getRandomIntInclusive(1, 4)
+		el.classList.add('size' + mod);
 	}
 
 	function make_alert(text, type) {
